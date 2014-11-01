d0725f12231fd06521c6b413cd8d694ed16e777d
Edilson Gonzalez <edilson.gonza@unitec.edu>
2014-10-31 19:35:39 -0600

    Campus ready to test

--- SII/Models/AccountModels.cs
+++ SII/Models/AccountModels.cs
@@ -24,13 +24,14 @@ namespace SII.Models
         [Key]
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
+        [Display(Name = "Usuario")]
         public string UserName { get; set; }
     }
 
     public class RegisterExternalLoginModel
     {
         [Required]
-        [Display(Name = "User name")]
+        [Display(Name = "Usuario")]
         public string UserName { get; set; }
 
         public string ExternalLoginData { get; set; }
@@ -40,17 +41,17 @@ namespace SII.Models
     {
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(Name = "Contraseña actual")]
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "La {0} debe tener al menos {2} caracteres de largo.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Nueva contraseña")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
+        [Display(Name = "Confirmar nueva contraseña")]
         [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
@@ -58,33 +59,33 @@ namespace SII.Models
     public class LoginModel
     {
         [Required]
-        [Display(Name = "User name")]
+        [Display(Name = "Usuario")]
         public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Contraseña")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Recuérdame")]
         public bool RememberMe { get; set; }
     }
 
     public class RegisterModel
     {
         [Required]
-        [Display(Name = "User name")]
+        [Display(Name = "Usuario")]
         public string UserName { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "La {0} debe tener al menos {2} caracteres de largo.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Contraseña")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden.")]
         public string ConfirmPassword { get; set; }
     }
 
--- SII/Models/BaseModel.cs
+++ SII/Models/BaseModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -9,10 +10,14 @@ namespace SII.Models
     public class BaseModel
     {
         public int Id { get; set; }
+        [Display(Name = "Creado por")]
         public String CreatedBy { get; set; }
+        [Display(Name = "Fecha de creación")]
         [Column(TypeName = "DateTime2")]
         public DateTime CreatedAt { get; set; }
+        [Display(Name = "Última actualización por")]
         public String UpdatedBy { get; set; }
+        [Display(Name = "Fecha de última actualización")]
         [Column(TypeName = "DateTime2")]
         public DateTime? UpdatedAt { get; set; }
         public bool Dropped { get; set; }
