68edc8761defe928f5ed20a36d53a96d3b721c21
Henry23 <gabriel_23hgap@unitec.edu>
2014-11-11 09:38:17 -0600

    Updates

--- SII.Tests/Models/AnnouncementModelTest.cs
+++ SII.Tests/Models/AnnouncementModelTest.cs
@@ -16,6 +16,35 @@ namespace SII.Tests.Models
             Assert.AreEqual(new DateTime(2013, 1, 1), announcement.UpdatedAt);
         }
 
+        [Test]
+        public void EnteringJust16CharacteresInObservations()
+        {
+            var announcement = new Announcement { Observations = "testing 01"};
+            Assert.IsTrue(announcement.Observations.Length <= 16 );
+        }
+
+        [Test]
+        public void EnteringJustNumersCharacteresInObservations()
+        {
+            var announcement = new Announcement { Observations = "testing 01" };
+            Assert.IsTrue(JustCharacteresAndNumbers(announcement.Observations));
+        }
+
+        public bool JustCharacteresAndNumbers(String sr)
+        {
+            if (String.IsNullOrEmpty(sr))
+                return false;
+            for (int i = 0; i < sr.Length; i++)
+            {
+                if (sr[i].Equals('*') || sr[i].Equals('-') || sr[i].Equals('+') || sr[i].Equals('/') 
+                   || sr[i].Equals('|') || sr[i].Equals('>') || sr[i].Equals('<') || sr[i].Equals(':')
+                   | sr[i].Equals('@') || sr[i].Equals('#') || sr[i].Equals('$') || sr[i].Equals('%')
+                   || sr[i].Equals('^') || sr[i].Equals('&') || sr[i].Equals('(') || sr[i].Equals(')') || sr[i].Equals('_') || sr[i].Equals('='))
+                    return false;
+            }
+            return true;
+        }
+
         
     }
 }
