f352798ae117a70d955924cf9f39077fe8159b03
Ariel Esquivel <ariel-esquivel@hotmail.com>
2014-12-02 17:41:05 -0600

    Views test update

--- SII.Tests/Views/scripts/cleanUser.sql
+++ SII.Tests/Views/scripts/cleanUser.sql
@@ -12,3 +12,10 @@ select @guardiaId = (select UserId from UserProfile where UserName = 'guardia');
 delete from webpages_UsersInRoles where UserId = @guardiaId;
 delete from webpages_Membership where UserId = @guardiaId;
 delete from UserProfile where UserId = @guardiaId;
+
+declare @guardia2Id int;
+select @guardia2Id = (select UserId from UserProfile where UserName = 'guardia2');
+
+delete from webpages_UsersInRoles where UserId = @guardia2Id;
+delete from webpages_Membership where UserId = @guardia2Id;
+delete from UserProfile where UserId = @guardia2Id;
