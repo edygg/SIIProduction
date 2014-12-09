aa5db9fa0e112c5b00e7b9159446db5ca71dd194
Ariel Esquivel <ariel-esquivel@hotmail.com>
2014-12-09 14:42:29 -0600

    Views test update

--- SII.Tests/Views/scripts/cleanData.sql
+++ SII.Tests/Views/scripts/cleanData.sql
@@ -1,13 +1,12 @@
 use [SII.Models.SIIContext];
 
 delete Departments;
+
 delete Entrances;
 
-delete from barreras where barrera = 'Barrera prueba';
-delete from barreras where barrera = 'Barrera prueba 2';
+delete barreras;
 
-delete from Visits where AnnouncementId > 1;
-delete from Announcements where id > 1;
+delete Visits;
+delete Announcements;
 
-delete from Campus where Code = 'UNITEG';
-delete from Campus where Code = 'UNISPS';
+delete Campus;
